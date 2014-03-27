056830fa2787fd8179ec3960d3dc7cbb6731ffc9
Milan Manwar <milan.manwar@gmail.com>
2014-03-27 11:08:16 +0530

    Initial Commit

---
+++ JsonKivaws/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JsonKivaws
+//
+//  Created by Omnipresent on 27/03/14.
+//  Copyright (c) 2014 Omnipresent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JsonKivaws/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JsonKivaws
+//
+//  Created by Omnipresent on 27/03/14.
+//  Copyright (c) 2014 Omnipresent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
